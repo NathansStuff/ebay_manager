0a42ec32a9b37975ba1d5e3b0c4293fbc5d99d80
NathansStuff <63134709+NathansStuff@users.noreply.github.com>
2021-07-16 04:54:57 +1000

    react added

--- ebaymanager/ebaymanager/urls.py
+++ ebaymanager/ebaymanager/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('dvds.urls')),
+    path('', include('frontend.urls')),
+    path('', include('dvds.urls')),
 ]
