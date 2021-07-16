00fead769fb542660b61fb53dd5f16fe62062d2d
NathansStuff <63134709+NathansStuff@users.noreply.github.com>
2021-07-16 15:18:23 +1000

    changed api urls to /api

--- ebaymanager/dvds/urls.py
+++ ebaymanager/dvds/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers, urlpatterns
 from .api import DvdViewSet
 
 router = routers.DefaultRouter()
-router.register('dvd', DvdViewSet, 'dvds')
+router.register('dvds', DvdViewSet, 'dvds')
 
 urlpatterns = router.urls
--- ebaymanager/frontend/src/actions/dvds.js
+++ ebaymanager/frontend/src/actions/dvds.js
@@ -4,7 +4,20 @@ import { GET_DVDS } from './types';
 // GET DVDS
 export const getDvds = () => dispatch => {
   axios
-    .get('/dvd/')
+    .get('/api/dvds/')
+    .then(res => {
+      dispatch({
+        type: GET_DVDS,
+        payload: res.data,
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// DELETE DVDS
+export const deleteDvds = (id) => dispatch => {
+  axios
+    .get('/api/dvds/')
     .then(res => {
       dispatch({
         type: GET_DVDS,
