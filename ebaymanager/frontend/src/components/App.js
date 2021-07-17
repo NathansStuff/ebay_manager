1893a42d11f2b58ab1da2882cf1e9363e8dff613
NathansStuff <63134709+NathansStuff@users.noreply.github.com>
2021-07-17 22:43:55 +1000

    frontend redux token checks private routes but no login fun

--- ebaymanager/frontend/src/components/App.js
+++ ebaymanager/frontend/src/components/App.js
@@ -11,12 +11,14 @@ import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 
 import store from '../store';
+import { loadUser } from '../actions/auth';
 
 import Header from './layout/Header';
 import Dashboard from './dvds/Dashboard';
 import Alerts from './layout/Alerts';
 import Login from './accounts/Login';
 import Register from './accounts/Register';
+import PrivateRoute from './common/PrivateRoute';
 
 //Alert Options
 const alertOptions = {
@@ -25,6 +27,10 @@ const alertOptions = {
 };
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
@@ -35,7 +41,7 @@ class App extends Component {
               <Alerts />
               <div className='container'>
                 <Switch>
-                  <Route exact path='/' component={Dashboard} />
+                  <PrivateRoute exact path='/' component={Dashboard} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
                 </Switch>
--- ebaymanager/frontend/src/reducers/index.js
+++ ebaymanager/frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import dvds from './dvds';
 import errors from './errors';
 import messages from './messages';
+import auth from './auth';
 
 export default combineReducers({
   dvds,
   errors,
   messages,
+  auth,
 });
