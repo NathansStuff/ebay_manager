0af40faa17330e03a8e95ea3dfb3bd0a8dabb8cf
NathansStuff <63134709+NathansStuff@users.noreply.github.com>
2021-07-22 04:46:46 +1000

    dvd show page route works but cannot be private

--- ebaymanager/frontend/src/components/App.js
+++ ebaymanager/frontend/src/components/App.js
@@ -13,12 +13,14 @@ import AlertTemplate from 'react-alert-template-basic';
 import store from '../store';
 import { loadUser } from '../actions/auth';
 
-import Header from './layout/Header';
-import Dashboard from './dvds/Dashboard';
 import Alerts from './layout/Alerts';
 import Login from './accounts/Login';
 import Register from './accounts/Register';
 import PrivateRoute from './common/PrivateRoute';
+
+import Header from './layout/Header';
+import Dashboard from './dvds/Dashboard';
+import Show from './dvds/Show';
 
 //Alert Options
 const alertOptions = {
@@ -42,6 +44,12 @@ class App extends Component {
               <div className='container'>
                 <Switch>
                   <PrivateRoute exact path='/' component={Dashboard} />
+                  <Route
+                    path='/:id'
+                    component={props => (
+                      <Show id={props.match.params.id} />
+                    )}
+                  />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
                 </Switch>
---
+++ ebaymanager/frontend/src/components/dvds/Show.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function DvdCard(props) {
+  return (
+    <div>
+      <h1>Show {props.id}</h1>
+    </div>
+  );
+}
